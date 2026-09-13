df3df47e215facd5b426e21a84f0befec1b78602
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-09-13 10:47:35 +0500

    Add dynamic grid generation

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log('Hello World! I am the brain of Sketchify');
+// selecting main Elements
+const grid = document.querySelector('#grid');
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+// Creating grid
+function createGrid(input) {
+    //clear out the previous grid completely
+    grid.innerHTML = '';
+    // Convert string value to a number
+    const value = parseInt(input.value);
+    for (let index = 0; index < value; index++) {
+        const createDivRow = document.createElement('div');
+        createDivRow.classList.add('grid-row'); 
+        grid.appendChild(createDivRow);
+    }
+}
+
+// creating Grid Items.
+function createGridItems(input) {
+    const value = input.value;
+    // selecting the rows as grid-row elements.
+    const divRows = document.querySelectorAll('.grid-row');
+    // iterating over each one of them.
+    divRows.forEach(divRow => {
+        // append to it a new div for as much time as the value of fetched from the input.
+         for(let index = 0; index < value; index ++) {
+            const createDivItem = document.createElement('div');
+            createDivItem.classList.add('grid-item'); // Added class for easier CSS styling
+            createDivItem.textContent = '';
+            divRow.appendChild(createDivItem);
+         }
+    });
+}
+
+// attach a single change event listener to each button.
+radioButtons.forEach((btn) => {
+    btn.addEventListener('change', (event) => {
+        // event.target is the specific radio btn that was selected
+        const selectedRadioButton = event.target;
+
+        // passing the radio button elements into functions
+        createGrid(selectedRadioButton);
+        createGridItems(selectedRadioButton);
+    });
+});
+
